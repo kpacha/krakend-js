d34e42b0acbab1434000bdee5975db7099f90924
kpacha <kpacha666@gmail.com>
2018-11-04 13:29:22 +0100

    config2dot package added

--- main.go
+++ main.go
@@ -1,36 +1,67 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"syscall/js"
 
+	"github.com/devopsfaith/krakend-config2dot"
 	"github.com/devopsfaith/krakend/config"
 )
 
-func parse(i []js.Value) {
-	println("parsing")
-	source := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+var document js.Value
 
-	cfg, err := config.NewParserWithFileReader(func(s string) ([]byte, error) {
-		return []byte(s), nil
-	}).Parse(source)
+func init() {
+	document = js.Global().Get("document")
+}
+
+func dotParser(i []js.Value) {
+	println("dotParser")
+
+	cfg, err := configParser(readElementValue(i[0].String()))
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
-	js.Global().Get("document").Call("getElementById", i[1].String()).Set("value", js.ValueOf(fmt.Sprintf("%+v", cfg)))
+	buf := new(bytes.Buffer)
+	dot.ServiceConfig(cfg).WriteTo(buf)
+
+	setElementValue(i[1].String(), buf.String())
+	js.Global().Call("draw", i[2], js.ValueOf(buf.String()))
 }
 
-func registerCallbacks() {
-	js.Global().Set("parse", js.NewCallback(parse))
+func parse(i []js.Value) {
+	println("parsing")
+
+	cfg, err := configParser(readElementValue(i[0].String()))
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	setElementValue(i[1].String(), fmt.Sprintf("%+v", cfg))
+}
+
+func configParser(source string) (config.ServiceConfig, error) {
+	return config.NewParserWithFileReader(func(s string) ([]byte, error) {
+		return []byte(s), nil
+	}).Parse(source)
+}
+
+func readElementValue(name string) string {
+	return document.Call("getElementById", name).Get("value").String()
+}
+
+func setElementValue(name, value string) {
+	document.Call("getElementById", name).Set("innerHTML", js.ValueOf(value))
 }
 
 func main() {
-	c := make(chan struct{}, 0)
-
 	println("WASM Go Initialized")
-	// register functions
-	registerCallbacks()
-	<-c
+
+	js.Global().Set("parse", js.NewCallback(parse))
+	js.Global().Set("dotParser", js.NewCallback(dotParser))
+
+	select {}
 }
