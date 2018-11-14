ee0ad11b4451e29a36fa72a2052364e86e7dd97c
kpacha <kpacha666@gmail.com>
2018-11-14 15:39:15 +0100

    external treemap lib added

--- config/config.go
+++ config/config.go
@@ -1,70 +1,70 @@
 package config
 
 import (
-	"encoding/json"
+	"context"
+	"fmt"
 	"math"
 	"net/http"
+	"sort"
 
 	"github.com/devopsfaith/krakend/config"
+	"github.com/kpacha/treemap"
 )
 
-func Parse(cfg config.ServiceConfig) *Block {
-	methods := map[string][]*Block{}
-	for _, e := range cfg.Endpoints {
-		var blocks []*Block
+func Parse(cfg config.ServiceConfig) *treemap.Block {
+	children := parseEndpoints(cfg.Endpoints)
+
+	for _, i := range sortedKeys(cfg.ExtraConfig) {
+		children = []*treemap.TreeInfo{newNothing(i, 4, 4, dimm3Val(4, 4), children...)}
+	}
+
+	b := newOK(cfg.Name, 1, 1, dimm3Val(1, 1), children...)
+	return b.Tree(context.Background())
+}
+
+func parseEndpoints(cfgs []*config.EndpointConfig) []*treemap.TreeInfo {
+	methods := map[string][]*treemap.TreeInfo{}
+	for _, e := range cfgs {
+		var blocks []*treemap.TreeInfo
 		var ok bool
 		if blocks, ok = methods[e.Method]; !ok {
-			blocks = []*Block{}
+			blocks = []*treemap.TreeInfo{}
 		}
 		methods[e.Method] = append(blocks, parseEndpoint(e))
 	}
 
-	children := []*Block{}
+	children := []*treemap.TreeInfo{}
 	for _, method := range httpMethods {
 		ms, ok := methods[method]
 		if !ok {
 			continue
 		}
-		b := newBlock(method, 0, 0, ms...)
-		b.Color = colorOK
-		children = append(children, b)
+		children = append(children, newOK(method, 1, 1, dimm3Val(1, 1), ms...))
 	}
-
-	for i := range cfg.ExtraConfig {
-		children = []*Block{newBlock(i, 4, 4, children...)}
-	}
-
-	b := newBlock(cfg.Name, 0, 0, children...)
-	b.GenerateChildrenPosition()
-	b.Color = colorOK
-	return b
+	return children
 }
 
-func parseEndpoint(cfg *config.EndpointConfig) *Block {
-	children := make([]*Block, len(cfg.Backend))
+func parseEndpoint(cfg *config.EndpointConfig) *treemap.TreeInfo {
+	children := make([]*treemap.TreeInfo, len(cfg.Backend))
 	for i, b := range cfg.Backend {
 		children[i] = parseBackend(b)
 	}
 	if t := len(children); t > 1 {
-		child := newBlock("Merge", 5*t, 5*t, children...)
-		child.Color = colorDanger
-		children = []*Block{child}
+		children = []*treemap.TreeInfo{newDanger("Merge", 5*t, 5*t, dimm3Val(5*t, 5*t), children...)}
 	}
-	b := newBlock("Proxy", 0, 0, children...)
-	b.Color = colorCaution
-	for i := range cfg.ExtraConfig {
-		b = newBlock(i, 4, 4, b)
+	headers := len(cfg.HeadersToPass) / 2
+	b := newCaution("Proxy", 2+headers, 2+headers, dimm3Val(2+headers, 2+headers), children...)
+	for _, i := range sortedKeys(cfg.ExtraConfig) {
+		b = newNothing(i, 4, 4, dimm3Val(4, 4), b)
 	}
 
-	b = newBlock(cfg.Endpoint, 5, 5, b)
-	b.Color = colorRegular
-	return b
+	return newRegular(cfg.Endpoint, 5, 5, dimm3Val(5, 5), b)
 }
 
-func parseBackend(cfg *config.Backend) *Block {
-	children := []*Block{}
-	for i := range cfg.ExtraConfig {
-		children = []*Block{newBlock(i, 4, 4, children...)}
+func parseBackend(cfg *config.Backend) *treemap.TreeInfo {
+	children := []*treemap.TreeInfo{}
+	for _, i := range sortedKeys(cfg.ExtraConfig) {
+		children = []*treemap.TreeInfo{newNothing(i, 4, 4, dimm3Val(4, 4), children...)}
 	}
 	complexity := cfg.ConcurrentCalls
 	if cfg.Group != "" {
@@ -74,57 +74,81 @@ func parseBackend(cfg *config.Backend) *Block {
 		complexity++
 	}
 	complexity += len(cfg.URLKeys)
-	child := newBlock(cfg.URLPattern, complexity+4, complexity+4, children...)
-	child.Color = colorCaution
+	child := newCaution(cfg.URLPattern, complexity+4, complexity+4, dimm3Val(complexity+4, complexity+4), children...)
 	if t := len(cfg.Host); t > 1 {
-		child = newBlock("LB", t, t, child)
-		child.Color = colorRegular
+		child = newRegular("LB", t, t, dimm3Val(t, t), child)
 	}
 	if len(cfg.Blacklist) > 0 || len(cfg.Whitelist) > 0 {
-		child = newBlock("Filter", len(cfg.Blacklist), len(cfg.Whitelist), child)
-		child.Color = colorCaution
+		child = newCaution("Filter", len(cfg.Blacklist), len(cfg.Whitelist), dimm3Val(len(cfg.Blacklist), len(cfg.Whitelist)), child)
 	}
 	if len(cfg.Mapping) > 0 {
-		child = newBlock("Mapping", 1, 1, child)
-		child.Color = colorCaution
+		child = newCaution(fmt.Sprintf("Mapping %s", cfg.URLPattern), 1, 1, dimm3Val(1, 1), child)
 	}
 	return child
 }
 
-func newBlock(name string, dimm1, dimm2 int, children ...*Block) *Block {
-	return &Block{
-		Name:     name,
-		Dimm1:    dimm1,
-		Dimm2:    dimm2,
-		Children: children,
-		Color:    colorNothing,
+func dimm3Val(dimm1, dimm2 int) int {
+	return int(math.Sqrt(float64(dimm1+1) * float64(dimm2+1)))
+}
+
+func newDanger(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	b := newTreeInfo(name, dimm1, dimm2, dimm3, children...)
+	b.Color = colorDanger
+	return b
+}
+
+func newCaution(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	b := newTreeInfo(name, dimm1, dimm2, dimm3, children...)
+	b.Color = colorCaution
+	return b
+}
+
+func newRegular(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	b := newTreeInfo(name, dimm1, dimm2, dimm3, children...)
+	b.Color = colorRegular
+	return b
+}
+
+func newOK(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	b := newTreeInfo(name, dimm1, dimm2, dimm3, children...)
+	b.Color = colorOK
+	return b
+}
+
+func newNothing(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	b := newTreeInfo(name, dimm1, dimm2, dimm3, children...)
+	b.Color = colorNothing
+	return b
+}
+
+func newTreeInfo(name string, dimm1, dimm2, dimm3 int, children ...*treemap.TreeInfo) *treemap.TreeInfo {
+	return &treemap.TreeInfo{BlockInfo: newBlock(name, dimm1, dimm2, dimm3), Children: children}
+}
+
+func newBlock(name string, dimm1, dimm2, dimm3 int) treemap.BlockInfo {
+	return treemap.BlockInfo{
+		Name:  name,
+		Dimm1: dimm1,
+		Dimm2: dimm2,
+		Dimm3: dimm3,
 	}
 }
 
-type Block struct {
-	Name     string   `json:"name"`
-	Depth    float64  `json:"depth"`
-	Width    float64  `json:"width"`
-	Children []*Block `json:"children,omitempty"`
-	Dimm1    int      `json:"dimm1"`
-	Dimm2    int      `json:"dimm2"`
-	Position Position `json:"position"`
-	Color    string   `json:"color,omitempty"`
-}
-
-type Position struct {
-	X float64 `json:"x"`
-	Y float64 `json:"y"`
+func sortedKeys(m map[string]interface{}) []string {
+	names := make([]string, 0, len(m))
+	for name := range m {
+		names = append(names, name)
+	}
+	sort.Strings(names) //sort by key
+	return names
 }
 
 const (
-	defaultMargin = 3
-
-	colorDanger  = "0xf6511d"
-	colorCaution = "0xffb400"
-	colorRegular = "0x00a6ed"
-	colorOK      = "0x7fb800"
-	colorNothing = "0x0d2c54"
+	colorDanger  = treemap.Color("0xf6511d")
+	colorCaution = treemap.Color("0xffb400")
+	colorRegular = treemap.Color("0x00a6ed")
+	colorOK      = treemap.Color("0x7fb800")
+	colorNothing = treemap.Color("0x0d2c54")
 )
 
 var (
@@ -140,85 +164,3 @@ var (
 		http.MethodTrace,
 	}
 )
-
-func (b *Block) String() string {
-	buf, _ := json.MarshalIndent(b, "", "\t")
-	return string(buf)
-}
-
-func (b *Block) GenerateChildrenPosition() {
-	if b.Children == nil || len(b.Children) == 0 {
-		b.Width = float64(b.Dimm1) + 3
-		b.Depth = float64(b.Dimm2) + 3
-		return
-	}
-
-	positionGenerator := NewGenerator(len(b.Children))
-	for _, child := range b.Children {
-		child.GenerateChildrenPosition()
-		child.Position = positionGenerator.NextPosition(child.Width, child.Depth)
-	}
-
-	bounds := positionGenerator.GetBounds()
-	b.Width, b.Depth = bounds.X, bounds.Y
-
-	for _, child := range b.Children {
-		child.Position.X -= b.Width / 2.0
-		child.Position.Y -= b.Depth / 2.0
-	}
-
-	b.Width += float64(b.Dimm1)
-	b.Depth += float64(b.Dimm2)
-}
-
-type generator struct {
-	numberNodes  int
-	margin       int
-	dimension    int
-	xReference   float64
-	yReference   float64
-	currentIndex int
-	maxWidth     float64
-	maxHeight    float64
-	bounds       Position
-}
-
-func NewGenerator(numberNodes int) *generator {
-	generator := &generator{
-		numberNodes: numberNodes,
-		dimension:   int(math.Ceil(math.Sqrt(float64(numberNodes)))),
-	}
-
-	return generator
-}
-
-func (g *generator) GetBounds() Position {
-	return Position{
-		X: g.maxWidth + defaultMargin,
-		Y: g.maxHeight + defaultMargin,
-	}
-}
-
-func (g *generator) NextPosition(width, height float64) Position {
-	g.currentIndex++
-
-	if g.currentIndex > g.dimension && g.yReference+height >= g.maxWidth {
-		g.currentIndex = 0
-		g.yReference = 0
-		g.xReference = g.maxWidth + defaultMargin
-	}
-
-	position := Position{X: g.xReference + (width+defaultMargin)/2, Y: g.yReference + (height+defaultMargin)/2}
-
-	if g.xReference+width > g.maxWidth {
-		g.maxWidth = g.xReference + width
-	}
-
-	if g.yReference+height > g.maxHeight {
-		g.maxHeight = g.yReference + height
-	}
-
-	g.yReference += height + defaultMargin
-
-	return position
-}
